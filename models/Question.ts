a66adeefd4659df91168b8114f03a5e3f94ab38e
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2021-01-24 02:20:47 +0900

    update

--- models/Question.ts
+++ models/Question.ts
@@ -4,7 +4,7 @@ export interface Question {
   id: string;
   senderUid: string;
   receiverUid: string;
-  body: string:
+  body: string;
   isReplied: boolean;
   createdAt: firebase.firestore.Timestamp;
 }
